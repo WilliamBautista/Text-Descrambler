3daab34f7d640b920d8813a803e1a21962169706
William Bautista <92639049+WilliamBautista@users.noreply.github.com>
2023-03-10 15:26:00 -0500

    Create Encrypter.java

---
+++ src/Encrypter.java
@@ -0,0 +1,29 @@
+import org.jasypt.util.text.*;
+import java.util.ArrayList;
+
+public class Encrypter{
+
+    public static ArrayList<String> de_list (ArrayList <String> list, String pass){
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        ArrayList<String> writ = new ArrayList<String>();
+
+        textEncryptor.setPassword(pass);
+
+        for(String str: list) {
+            String deText = textEncryptor.decrypt(str);
+            writ.add(deText);
+          }
+          return writ;
+    }
+    public static ArrayList<String> en_list (ArrayList <String> list, String pass){
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        ArrayList<String> writ = new ArrayList<String>();
+
+        textEncryptor.setPassword(pass);
+
+        for(String str: list) {
+            String enText = textEncryptor.encrypt(str);
+            writ.add(enText);
+          }
+          return writ;
+    }
