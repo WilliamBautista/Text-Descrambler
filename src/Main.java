9ccb69c7e28173e29eb6b321b4ec5a7bfb3184db
William Bautista <92639049+WilliamBautista@users.noreply.github.com>
2023-03-23 16:49:39 -0400

    Update Main.java
    - Added the ability to view a file and now also edit a line in the file to make any changes to the file as needed
    - Added error checking
    - As of this patch, it is safe to say that the app is able to complete the main actions required
    - Much work still needs to be done to refine the project

--- src/Main.java
+++ src/Main.java
@@ -1,121 +1,164 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import org.jasypt.util.text.*;
 import java.io.File;
 
-
 public class Main {
 
-    public static void main(String[] args){
+  public static void main(String[] args) {
 
-        String pass;
-        String f_name;
-        Scanner input = new Scanner(System.in);
-        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+    String pass, f_name, choice, change, line;
+    int num;
+    Scanner input = new Scanner(System.in);
+    BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
 
-        ArrayList<String> writ = new ArrayList<String>();
-        ArrayList<String> detext = new ArrayList<String>();
+    ArrayList < String > writ = new ArrayList < String > ();
+    ArrayList < String > detext = new ArrayList < String > ();
 
+    System.out.println("******************************************************\n");
 
-        System.out.println("******************************************************\n");
-        
-        System.out.println("Hello welocome to the the file encrypter writer.\nPress enter to begin!");
-        System.out.println("If you would like to gain access to a file, please enter: View\n");
-        System.out.println("If you would like to quit the program, please type: No\n");
+    System.out.println("Hello welocome to the the file encrypter writer.\nPress enter to begin!");
+    System.out.println("If you would like to gain access to a file, please enter: View\n");
+    System.out.println("If you would like to quit the program, please type: No\n");
 
-        System.out.println("******************************************************\n");
-        
+    System.out.println("******************************************************\n");
 
-        String choice = input.nextLine().toUpperCase();
+    choice = input.nextLine().toUpperCase();
 
-        if(choice.toUpperCase().equals("NO")){
-            System.out.println("Ok goodbye!");
-            System.exit(0);
-        } else if(choice.toUpperCase().equals("VIEW")){
+    if (choice.toUpperCase().equals("NO")) {
+      System.out.println("Ok goodbye!");
+      System.exit(0);
+    } else if (choice.toUpperCase().equals("VIEW")) {
 
-            System.out.println("Please enter the name of the file. Do not include file ending.\n");
+      System.out.println("Please enter the name of the file. Do not include file ending.\n");
 
-            f_name = input.nextLine()+".txt";
+      f_name = input.nextLine() + ".txt";
 
-            while(!new File("#"+f_name).isFile()){
-                System.out.print("Please try again with a different name.\n\n");
-                f_name = input.nextLine()+".txt";
+      while (!new File("directory" + f_name).isFile()) {
 
-            }
-            System.out.print("File name is accepted.\n");
+        System.out.print("Please try again with a different name.\n\n");
+        f_name = input.nextLine() + ".txt";
 
-            writ = Writer.get_text(f_name);
+      }
+      System.out.print("File name is accepted.\n");
 
-            System.out.print("Please enter the password of the file you want to accesss.\n");
-            
-            pass = input.nextLine();
+      writ = Writer.get_text(f_name);
 
-            detext = Encrypter.de_list(writ, pass);
+      System.out.print("Please enter the password of the file you want to accesss.\n");
 
-            int i = 1;
+      pass = input.nextLine();
 
-            try{
-                for(String str: detext) {
-                    System.out.println(i+" "+str);
-                    i++;
-                }
-            }catch(Exception e){
-                System.out.println("Error: password is incorrect. Program will shut down");
-                System.exit(0);
-            }
-            System.exit(0);
+      detext = Encrypter.de_list(writ, pass);
+
+      int i = 0;
+
+      try {
+        for (String str: detext) {
+          System.out.println(i + " " + str);
+          i++;
+        }
+      } catch (Exception e) {
+        System.out.println("Error: password is incorrect. Program will shut down");
+        System.exit(0);
+      }
+      System.out.println("\nIf you woulld like to edit the file " +
+        "you will have to provide the line of the file edit\nOtherwise type" +
+        " Done to finish viewing");
+      choice = input.nextLine();
+
+      while (!choice.toUpperCase().equals("DONE")) {
+
+        i = 0;
+
+        for (String str: detext) {
+          System.out.println(i + " " + str);
+          i++;
+        }
+        System.out.println("\nPlease select a line number from the ones listed above");
+        try {
+          num = input.nextInt();
+
+        } catch (InputMismatchException e) {
+          System.out.println("Input is not a number please try again!");
+          num = input.nextInt();
+        }
+        input.nextLine();
+
+        System.out.println("\nPlease write what you would like to replace");
+        change = input.nextLine();
+
+        try {
+          detext.set(num, change);
+          System.out.println("\nChange has been made would you like to continue?\nPress enter to continue or type: Done to quit");
+
+        } catch (Exception e) {
+          System.out.println("That line number does not exist");
+          System.out.println("Try again? Please hit enter else type: Done");
         }
 
-        System.out.println("Please enter the name of the file.\n");
+        choice = input.nextLine();
+      }
 
-        f_name = input.nextLine()+".txt";
+      textEncryptor.setPassword(pass);
 
-        while (!Writer.create_file(f_name)){
-            System.out.print("Please try again with a different name.\n\n");
-            f_name = input.nextLine()+".txt";
-        }
+      Writer.empty(f_name);
 
-
-        System.out.println("The next step is to now to crete a password that will lock your file.");
-
-        System.out.print("\nA password must have:\n Must contain at least eight characters\n" +
-            " Must contain letters, digits, and a special character\n Must contain at lease a digit\n\n");
-
-        pass = input.nextLine();
-        
-        while (!Pass_create.password_checker(pass)) {
-
-            System.out.print("\nNot a valid password!\n");
-            System.out.print("Please try again.\n\n");
-            pass = input.nextLine();
-            
-        }
-        System.out.print("\nPasswword has been accepted\n\n");
-
-
-        System.out.println("Next, please write into file:\nIf you would like to stop type: Done\n");
-
-        textEncryptor.setPassword(pass);
-
-        while(true){
-
-          String line = input.nextLine();
-
-          if (line.toUpperCase().equals("DONE")){
-            break;
-          }
-          writ.add(line);
-        }
-        
-        for(String str: writ) {
-
-            String enText = textEncryptor.encrypt(str);
-            Writer.write_file(enText+"\n", f_name);
-
-        }
-
-        System.out.println("Thank you very much, your file has been created and encrypted");
-        
-        input.close();
+      for (String str: detext) {
+        String enText = textEncryptor.encrypt(str);
+        Writer.write_file(enText + "\n", f_name);
+      }
+      System.exit(0);
     }
+
+    System.out.println("Please enter the name of the file.\n");
+
+    f_name = input.nextLine() + ".txt";
+
+    while (!Writer.create_file(f_name)) {
+      System.out.print("Please try again with a different name.\n\n");
+      f_name = input.nextLine() + ".txt";
+    }
+
+    System.out.println("The next step is to now to crete a password that will lock your file.");
+
+    System.out.print("\nA password must have:\n Must contain at least eight characters\n" +
+      " Must contain letters, digits, and a special character\n Must contain at lease a digit\n\n");
+
+    pass = input.nextLine();
+
+    while (!Pass_create.password_checker(pass)) {
+
+      System.out.print("\nNot a valid password!\n");
+      System.out.print("Please try again.\n\n");
+      pass = input.nextLine();
+
+    }
+    System.out.print("\nPasswword has been accepted\n\n");
+
+    System.out.println("Next, please write into file:\nIf you would like to stop type: Done\n");
+
+    textEncryptor.setPassword(pass);
+
+    while (true) {
+
+      line = input.nextLine();
+
+      if (line.toUpperCase().equals("DONE")) {
+        break;
+      }
+      writ.add(line);
+    }
+
+    for (String str: writ) {
+
+      String enText = textEncryptor.encrypt(str);
+      Writer.write_file(enText + "\n", f_name);
+
+    }
+
+    System.out.println("Thank you very much, your file has been created and encrypted");
+
+    input.close();
+  }
 }
